57a4deced56a8bae81216153a1ae60971bb3efde
sorpa'as plat <sorpaas@gmail.com>
2014-05-12 20:55:25 +0800

    add heist

--- lr-console.rb
+++ lr-console.rb
@@ -1,15 +1,23 @@
 require 'io/console'
+require 'heist'
+
+puts "(lr-console"
+puts " (author . sorpaas)"
+puts " (version . v0.01)"
+
+scheme = Heist::Runtime.new
 
 while(true)
   #deal with a line
   line_content = ""
   while(true)
-    print "\033[0K\r(#{line_content})\b"
+    print "\n)\033[1A\033[0K\r(#{line_content})\b"
     next_char = STDIN.getch
     if next_char == "\r"
       puts
-      puts "Result: #{line_content}"
       break
+    elsif next_char == "\u007F"
+      line_content = line_content[0, line_content.length - 1]
     else
       line_content += next_char
     end
@@ -17,5 +25,7 @@ while(true)
   
   if line_content.strip == "exit"
     break
+  else
+    puts scheme.eval "(#{line_content})"
   end
 end
